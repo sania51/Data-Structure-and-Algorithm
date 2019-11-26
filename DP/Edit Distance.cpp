610e13854d3e50c6945aa94254c104e1801956f3
Sania7 <42738243+Sania7@users.noreply.github.com>
2019-11-26 19:09:00 +0600

    Update Edit Distance.cpp

--- DP/Edit Distance.cpp
+++ DP/Edit Distance.cpp
@@ -8,13 +8,17 @@ int edit_distance(string source, string destination, int l1, int l2)
 {
     for(i=0; i<=l1; i++){
         for(j=0; j<=l2; j++){
+
             // if source is empty then insert all characters of destination
             if(i==0) dp[i][j] = j;
+
             // if destination is empty then delete all characters of destination
             else if(j==0) dp[i][j] = i;
-            /* dp[][] will be diagonal value coz no need to convert same char
-            diagonal value means edit distance for edit distance without that char */
+
+            /* dp[][] will be diagonal value coz no need to convert same char.
+            diagonal value means edit distance before that char */
             if(source[i-1] == destination[j-1]) dp[i][j] = dp[i-1][j-1];
+
             // consider all possibilities and take min
             else if (source[i-1] != destination[j-1]) dp[i][j] = 1+min(min(dp[i][j-1], dp[i-1][j]), dp[i-1][j-1]);
         }
